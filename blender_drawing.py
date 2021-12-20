7714bffb4f20c983658babb1d23f585d9108759d
AegeanKI <pmes.986612@gmail.com>
2021-12-20 20:18:42 +0800

    adjust blender_drawing camera intrinsic matrix

--- blender_drawing.py
+++ blender_drawing.py
@@ -5,14 +5,14 @@ import bpy
 bproc.init()
 
 # Create a simple object:
-#obj = bproc.object.create_primitive("MONKEY")
+# obj = bproc.object.create_primitive("MONKEY")
 obj2 = bpy.ops.import_mesh.stl(filepath="/Users/shenchi/Documents/code/python/exercise/chamber_project/data/chamber.stl")
 
 
 # Create a point light next to it
-#light = bproc.types.Light()
-#light.set_location([2, -2, 0])
-#light.set_energy(300)
+# light = bproc.types.Light()
+# light.set_location([0, 1400, 0])
+# light.set_energy(300)
 
 # Set the camera to be in front of the object
 cam_pose = bproc.math.build_transformation_mat([0, 1355, -20], [np.pi / 2, 0, -np.pi])
@@ -20,6 +20,29 @@ bproc.camera.add_camera_pose(cam_pose)
 
 bpy.context.scene.camera.data.clip_end = 10000
 bpy.context.scene.camera.data.sensor_width = 100
+
+cam = bpy.data.objects["Camera"].data
+scene = bpy.context.scene
+
+intrinsic = [[1.28532115e+03, 0.00000000e+00, 7.99553411e+02],
+			 [0.00000000e+00, 1.29038353e+03, 4.74003146e+02],
+			 [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]]
+
+f_x = intrinsic[0][0]
+f_y = intrinsic[1][1]
+c_x = intrinsic[0][2]
+c_y = intrinsic[1][2]
+w = 1536
+h = 960
+sensor_width_in_mm = cam.sensor_width
+
+cam.shift_x = -(c_x / w - 0.5)
+cam.shift_y = (c_y - 0.5 * h) / w
+
+cam.lens = f_x / w * sensor_width_in_mm
+pixel_aspect = f_y / f_x
+scene.render.pixel_aspect_x = 1.0
+scene.render.pixel_aspect_y = pixel_aspect
 
 # Render the scene
 data = bproc.renderer.render()
